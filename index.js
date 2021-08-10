6c559c093fa64072c50bad78ca0b2012a811986d
Hyrondelle <n.hyr@free.fr>
2021-08-10 17:40:31 +0200

    affichage produits

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 
 const url = "http://localhost:3000/api/teddies";
-const imgOurs = document.getElementById('image');
-const nameOurs = document.getElementById('name');
-
-console.log(imgOurs)
-
+const allOurs = document.getElementById('products');
+const errorServer = document.getElementById('error');
+const card = document.createElement('div');
+console.log(allOurs)
+                     //affichage des produits
 fetch(url)
     .then((rep) => 
          rep.json())
     .then((data) => {
         try{
             for(let i=0;i<=data.length;i++){  
-                let name = data[i].name;
-                 let photo = data[i].imageUrl;
-                 imgOurs.innerHTML += `<img src=${photo} height="200px" width= "250px">`;
-                 const p = document.createElement("p")
-                 imgOurs.appendChild(p);
-                 p.innerHTML += name;
+                oursName = data[i].name;
+                photo = data[i].imageUrl;
+                carteOurs = document.createElement("div");
+                carteOurs.className ='ours';
+                allOurs.appendChild(carteOurs);
+                carteOurs.innerHTML += `<img src=${photo} height="200px" width= "250px">`;
+                nomOurs = document.createElement("p")
+                carteOurs.appendChild(nomOurs);
+                nomOurs.innerHTML += oursName;
             }  
         }
         catch(e){
             console.log(e);
         }      
     })
-    .catch((e) => {
+    .catch((e) => {     //affichage message si probleme server
         console.log(e)
-        nameOurs.innerHTML = "aucun produit disponible pour le moment"
+        errorServer.innerHTML = "aucun produit disponible pour le moment"
     });
+                    //affichage d'un produit    
+    allOurs.addEventListener('click',() => {
+        card.id ='card';
+        document.body.appendChild(card);
+        card.appendChild(nomOurs);
+    })              //fermeture page produit
+    card.addEventListener('click',()=>{
+        card.remove();
+    })
+    
