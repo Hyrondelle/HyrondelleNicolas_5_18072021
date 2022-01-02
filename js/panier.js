d907dedcab55f87462aa8f31bd984c02ed2158f2
Hyrondelle <n.hyr@free.fr>
2022-01-02 23:21:47 +0100

    panier

--- js/panier.js
+++ js/panier.js
@@ -1,6 +1,7 @@
 const produits = document.getElementById('produits');
 const totalPanier = document.getElementById('total');
 const commande = document.getElementById('commande');
+const errorForm = document.getElementById('errorForm');
 let nbProduits = 0;
 let total =0;
 products = [];
@@ -37,6 +38,12 @@ commande.addEventListener('click',(event)=>{
         city: document.getElementById('ville').value,
         email: document.getElementById('email').value,
     }
+    if(contact.firstName==""||contact.lastName==""||contact.address==""||
+        contact.city==""||contact.email==""){
+        errorForm.textContent = "vous devez remplir tous les champs";
+        return;
+    }
+    errorForm.textContent = "";
     const contactProducts={contact,products};
     fetch('http://localhost:3000/api/teddies/order',{
         method:'POST',
@@ -47,7 +54,67 @@ commande.addEventListener('click',(event)=>{
     .then(data=>console.log(data))
     .catch(e=>console.log(e));
 })
-const firstName=  document.getElementById('prenom');
-firstName.addEventListener('input',(e)=>{
-    console.log(e.target.value);
-})
+let inputs = document.querySelectorAll('input');
+inputs.forEach(input =>
+    input.addEventListener('input',(e)=>{
+    switch(e.target.id){
+        case "prenom":
+        case  "nom":
+        case "ville":prenomNomVilleChecker(e.target.value,e.target.id);
+            break;
+        case "adresse":adresseChecker(e.target.value,e.target.id);
+            break;
+        case "email":emailChecker(e.target.value,e.target.id);
+            break;
+        }           
+    }));
+    
+const displayError = (inputId,message,valid)=>{
+    const elementId = document.getElementById(inputId);
+    const divError = document.querySelector("."+inputId+" > div");
+    if(!valid){
+        divError.textContent=message;
+        elementId.classList.add('error');
+    }
+    else{
+        divError.textContent=message;
+        elementId.classList.remove('error');
+    }
+}
+const prenomNomVilleChecker = (value,inputId) =>{
+    if(value.length == 0){
+        displayError(inputId,"");
+    }
+    else if(value.length >0 && (value.length <3 ||value.length >20)){
+        displayError(inputId,"3 caractères min et 20 maximum");
+    }
+    else if(!value.match(/^[a-zA-Z\-]+$/)){
+        displayError(inputId,"ne doit pas contenir de chiffres ou de caractères spéciaux");
+    }
+    else{
+        displayError(inputId,"",true);
+    }
+}
+const adresseChecker = (value,inputId) =>{
+    if(value.length == 0){
+        displayError(inputId,"");
+    }
+    else if(!value.match(/^[a-z0-9\s,'-]*$/i)){
+        displayError(inputId,"ne doit pas contenir de caractères spéciaux");
+    }
+    else{
+        displayError(inputId,"",true);
+    }
+}
+const emailChecker = (value,inputId) =>{
+    if(value.length == 0){
+        displayError(inputId,"");
+    }
+    else if(!value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
+        displayError(inputId,"email non valide");
+    }
+    else{
+        displayError(inputId,"",true);
+    }
+}
+
